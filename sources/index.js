a33bc89a63b5ecc203dabbe19abd5a0116a38199
Your Karim DAHOUMANE <krmdahoumane@gmail.com>
2022-01-20 00:38:49 +0100

    Backend : Implemented the draw function by counting the points according to the right card

--- sources/index.js
+++ sources/index.js
@@ -1,16 +1,24 @@
 //import { computeScore } from "./score";
 
 (async () => {
+  const drawCardElement = document.getElementById("btn");
+  const scoreElement = document.getElementById("score");
+  let scoreValue = null;
   let deckId = await getDeckId();
-  let carte = await drawCard(deckId);
-  console.log(carte);
+  drawCardElement.addEventListener("click", async () => {
+    scoreValue = scoreElement.textContent;
+    console.log("Valeur avant d'avoir pioché : " + scoreValue);
+    let cardCode = await drawCard(deckId);
+    let score = await computeScore(cardCode, scoreValue);
+    scoreElement.textContent = score;
+  });
 })();
 
 async function getDeckId() {
   let deckId = fetch(`https://deckofcardsapi.com/api/deck/new/`)
     .then((res) => res.json())
-    .then((response) => {
-      return response.deck_id;
+    .then((res) => {
+      return res.deck_id;
     });
   return deckId;
 }
@@ -24,4 +32,17 @@ async function drawCard(deckId) {
       return res.cards[0].code;
     });
   return result;
+}
+
+async function computeScore(cardCode, scoreValue) {
+  let cardParameters = cardCode.split("");
+  let cardValue = cardParameters[0];
+  switch (cardValue) {
+    case "A":
+      return parseInt(scoreValue) + 0;
+    case "J" || "Q" || "K":
+      return parseInt(scoreValue) + 10;
+    default:
+      return parseInt(scoreValue) + parseInt(cardValue);
+  }
 }
