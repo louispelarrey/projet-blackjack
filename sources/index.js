a7e0eb2133662c70b8f413639f6a0fe99a50491e
Your Karim DAHOUMANE <krmdahoumane@gmail.com>
2022-01-20 01:10:16 +0100

    Backend : Implemented the score rules (win or lose)

--- sources/index.js
+++ sources/index.js
@@ -1,18 +1,47 @@
-//import { computeScore } from "./score";
-
 (async () => {
-  const drawCardElement = document.getElementById("btn");
+  const drawCardElement = document.getElementById("btn-draw");
+  const stopGameElement = document.getElementById("btn-stop");
+  const informationElement = document.getElementById("gameResult");
   const scoreElement = document.getElementById("score");
+  let stop = false;
   let scoreValue = null;
   let deckId = await getDeckId();
   drawCardElement.addEventListener("click", async () => {
+    stop = false;
     scoreValue = scoreElement.textContent;
-    console.log("Valeur avant d'avoir pioché : " + scoreValue);
     let cardCode = await drawCard(deckId);
     let score = await computeScore(cardCode, scoreValue);
     scoreElement.textContent = score;
+    informationElement.textContent = await gameResult(score, stop);
+  });
+  stopGameElement.addEventListener("click", async () => {
+    stop = true;
+    scoreValue = scoreElement.textContent;
+    let cardCode = await drawCard(deckId);
+    let score = await computeScore(cardCode, scoreValue);
+    scoreElement.textContent = score;
+    console.log(stop);
+    informationElement.textContent = await gameResult(score, stop);
   });
 })();
+
+async function gameResult(score, stop) {
+    if (stop == true) {
+        if (score > 21) {
+            return "You won ! :D";
+        } else {
+            return "You lost... :(";
+        }
+    } else {
+        if (score == 21) {
+            return "You won ! :D";
+        } else if (score > 21) {
+            return "You lost";
+        } else {
+            return "Game in progress."
+        }
+    }
+}
 
 async function getDeckId() {
   let deckId = fetch(`https://deckofcardsapi.com/api/deck/new/`)
